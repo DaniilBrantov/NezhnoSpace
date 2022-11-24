4f426fc854ea3b0ab228e2ba9634f2aa89843d05
svetlana <svetlana_el12@mail.ru>
2022-11-24 17:43:05 +0500

    update: theme-text.js (data base)

--- js/theme-text.js
+++ js/theme-text.js
@@ -1,377 +1,217 @@
 (() => {
-  const arrayThemes = {
-    1: [
-      {
-        title: "Общее",
-        text: `Тема 1: Общее  Цель гайда предложить тебе книги, фильмы, проекты, подкасты по принципу -
-        ровно столько, сколько нужно.
-        less is more Мозгу так легче сфокусироваться и сделать первый шаг.
-        Цель гайда предложить тебе книги, фильмы, проекты, <span>подкасты</span>
-        по принципу - ровно столько, сколько нужно. less is more. Мозгу так легче сфокусироваться и
-        сделать
-        первый
-        шаг. Гайды Нежно Space - это не список литературы на лето. Ты можешь ограничиться цитатами
-        из книг,
-        которые`,
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 1: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 1: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 1: Тренажер",
-      },
-    ],
-    2: [
-      {
-        title: "Общее",
-        text: "Тема 2: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 2: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 2: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 2: Тренажер",
-      },
-    ],
-    3: [
-      {
-        title: "Общее",
-        text: "Тема 3: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 3: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 3: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 3: Тренажер",
-      },
-    ],
-    4: [
-      {
-        title: "Общее",
-        text: "Тема 4: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 4: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 4: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 4: Тренажер",
-      },
-    ],
-    5: [
-      {
-        title: "Общее",
-        text: "Тема 5: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 5: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 5: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 5: Тренажер",
-      },
-    ],
-    6: [
-      {
-        title: "Общее",
-        text: "Тема 6: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 6: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 6: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 6: Тренажер",
-      },
-    ],
-    7: [
-      {
-        title: "Общее",
-        text: "Тема 7: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 7: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 7: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 7: Тренажер",
-      },
-    ],
-    8: [
-      {
-        title: "Общее",
-        text: "Тема 8: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 8: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 8: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 8: Тренажер",
-      },
-    ],
-    9: [
-      {
-        title: "Общее",
-        text: "Тема 9: Общее",
-      },
-      {
-        title: "Рекомендуемая система",
-        text: "Тема 9: Рекомендуемая система",
-      },
-      {
-        title: "Дневник питания",
-        text: "Тема 9: Дневник питания",
-      },
-      {
-        title: "Тренажер",
-        text: "Тема 9: Тренажер",
-      },
-    ],
-  };
-
-  //Переменные
   const list = document.querySelector(".trial_list");
-  const text = document.querySelector(".trial_description-text");
+  const text = document.querySelector(".trial_description");
 
-  //Выполнение функции при загрузке DOM
-  document.addEventListener("DOMContentLoaded", function () {
-    const subtitle = document.querySelectorAll(".trial_nested-list li");
-    //Вывод объекта data из бд
-    $.getJSON("processing", function (data) {
-      // try_free_theme - это Тема,нажатая пользователем
-      let try_free_theme = [];
+  const dataBase = {};
 
-      // Перебор объекта из бд.
-      function EnumerationData(li) {
-        $.each(data, function (i, val) {
-          if (val.title == li.id) {
-            try_free_theme.push(val);
+  function activeElement(array, elem, activeClass) {
+    array.forEach((arr) => {
+      if (arr !== elem) arr.classList.remove(activeClass);
+    });
+    elem.classList.add(activeClass);
+  }
+
+  //функция на изменение контента
+  function changeContent(elem) {
+    //удаление текущего текста
+    text.querySelector('.trial_description-text').innerHTML = '';
+    text.querySelector('.trial_text-wrap').innerHTML = '';
+
+    //1 вариант
+    if (elem.text.length > 600) {
+      let firstText, lastText;
+
+      //делим текст примерно пополам и рубим его по пробелу
+      for (let i = Math.floor(elem.text.length / 2); i > 0; i--) {
+        if (elem.text[i] === ' ') {
+          firstText = elem.text.substring(0, i);
+          lastText = elem.text.substring(i + 1)
+          break;
+        }
+      }
+  
+      let firstArray = firstText.split('  ');
+  
+      for (let i = 0; i < firstArray.length; i++) {
+        text.querySelector('.trial_description-text').innerHTML += `
+          <p>${firstArray[i]}</p>
+        `;
+      }
+  
+      let lastArray = lastText.split('  ');
+  
+      for (let i = 0; i < lastArray.length; i++) {
+        text.querySelector('.trial_text-wrap').innerHTML += `
+          <p class='trial_text ${i === 0 ? 'trial_text-show' : ''}'>${lastArray[i]}</p>
+        `;
+      }
+    } else {
+      let arrayText = elem.text.split('  ');
+      if (arrayText) {
+        for (let i = 0; i < arrayText.length; i++) {
+          text.querySelector('.trial_description-text').innerHTML += `
+            <p>${arrayText[i]}</p>
+          `;
+        }
+      } else {
+        text.querySelector('.trial_description-text').innerHTML += `
+        <p>${elem.text}</p>
+      `;
+      }
+    }
+
+    //смена заголовка темы
+    document.querySelector('.trial_description-title').innerText = elem.theme;
+
+    //смена аудиофайла и его остановка 
+    document.querySelector('audio').pause();
+
+    if (!document.querySelector(".play")) {
+      document.querySelector(".pause").classList.add('play');
+      document.querySelector(".pause").classList.remove('pause');
+    }
+
+    document.querySelector('.player_title_text').innerText = elem.audio;
+    document.querySelector('audio').src = `wp-content/themes/my-theme/audio/${elem.audio}.mp3`;
+  }
+
+  $.getJSON("processing",
+    function (data) {
+      //определение количества тем
+      for (let key in data) {
+        dataBase[data[key].title] = [];
+      }
+      //форирование объекта с данными из полученных
+      for (let id in dataBase) {
+        for (let key in data) {
+          if (data[key].title == id) {
+            dataBase[data[key].title].push(
+              {
+                title: data[key].title,
+                text: data[key].text,
+                audio: data[key].audio,
+                theme: data[key].trial_theme
+              }
+            )
           }
-        });
+        }
       }
-
-      //Разделение текста на обзацы
-      var arrOfTxt;
-      function SeparationTxt(txtToSplit) {
-        arrOfTxt = txtToSplit.split('  ');
-        return arrOfTxt
+  }).then(function() {
+    //отрисовываем темы после полной обработки полученных данных 
+    for (let key in dataBase) {
+      list.innerHTML += `
+        <li class="trial_item" id=${key}>
+            <p data-key=${key} class="trial_title ${key == 1 ? "active" : ""}">Тема ${key}</p>
+              <ul class="trial_nested-list ${key == 1 ? "active" : ""}">
+                <li class="show-active">
+                    <p>Общее</p>
+                </li>
+                <li>
+                    <p class="trial_list-reference">Рекомендуемая система</p>
+                </li>
+                <li>
+                    <p class="trial_list-diary">Дневник питания</p>
+                </li>
+            </ul>
+        </li>
+      `;
+    }
+    //по умолчанию выставляется первая тема "общее"
+    dataBase[1].forEach((elem) => {
+      if (elem.theme === 'Общее') {
+        changeContent(elem);
       }
+    })
+  }).then(function() {
+    //вешаем обработчики после отрисовки DOM
+    const $window = $(window);
 
+    //desktop
+    if ($window.width() >= 960) {
+      const desktopTitle = document.querySelectorAll('.trial_title');
+      const desktopSubtitle = document.querySelectorAll(".trial_nested-list li");
 
-      //desktop
-      subtitle.forEach((li) => {
-        li.addEventListener("click", function () {
-          text.querySelectorAll("p").forEach((el) => { el.remove() });
-          EnumerationData(li.closest(".trial_item"));
+      desktopTitle.forEach((li) => {
+        li.addEventListener('click', function() {
+          //выделение активного заголовка
+          li.closest('.trial_item').querySelector('.trial_nested-list').classList.toggle('active');
+          li.classList.toggle('active');
 
-          //Определение активной подтемы
-          try_free_theme.forEach((elem) => {
-            if (elem.trial_theme === li.innerText) {
-              SeparationTxt(elem.text).forEach((e) => {
-                let create_txt = document.createElement('p');
-                create_txt.innerHTML = e;
-                text.append(create_txt);
-                try_free_theme = []
-              })
-              //text.querySelector("p").textContent = SeparationTxt(elem.text);
+          //скрытие неактивного заголовка
+          desktopTitle.forEach((title) => {
+            if (title !== li && title.classList.contains('active')) {
+              title.classList.remove('active');
+              title.closest('.trial_item').querySelector('.trial_nested-list').classList.remove('active');
+            }
+          })
+
+          //смена контента в зависимости от активного подзаголовка
+          li.closest('.trial_item').querySelectorAll('.trial_nested-list li').forEach((item) => {
+            if (item.classList.contains('show-active')) {
+              dataBase[li.closest(".trial_item").id].forEach((elem) => {
+                if (elem.theme === item.querySelector('p').textContent) {
+                  changeContent(elem);
+                }
+              });
+            }
+          })
+        })
+      });
+      desktopSubtitle.forEach((li) => {
+        li.addEventListener("click", function () { 
+          //выделение активного подзаголовка
+          activeElement(li.closest(".trial_item").querySelectorAll('.trial_nested-list li'), li, 'show-active');
+          //смена текста при клике на подзаголовок
+          dataBase[li.closest(".trial_item").id].forEach((elem) => {
+            if (elem.theme === li.innerText) {
+              changeContent(elem);
             }
           });
         });
       });
+    } else if ($window.width() < 960) {
+      const mobileTitle = document.querySelectorAll('.trial_title');
+      const mobileSubtitle = document.querySelectorAll(".trial_nested-list_mobile li");
 
-      //dropdown
-      var $window = $(window);
-      //mobile
-
-      const mobileSubtitle = document.querySelectorAll(
-        ".trial_nested-list_mobile li"
-      );
-      const mobileTitle = document.querySelectorAll(".trial_title");
-
-      if ($window.width() < 960) {
-        mobileSubtitle.forEach((li) => {
-          li.addEventListener("click", function () {
-            EnumerationData(list.querySelector(".trial_title.active"));
-
-            //Определение активной подтемы
-            try_free_theme.forEach((elem) => {
-              if (elem.trial_theme === li.innerText) {
-                mobileSubtitle.forEach((arr) => {
-                  if (arr !== elem) arr.querySelector("p").classList.remove("active");
-                });
-                li.querySelector("p").classList.add("active");
-                SeparationTxt(elem.text).forEach((e) => {
-                  let create_txt = document.createElement('p');
-                  create_txt.innerHTML = e;
-                  text.append(create_txt);
-                  try_free_theme = []
-                })
-                //text.querySelector("p").textContent = SeparationTxt(elem.text);
-              }
-            });
-          });
-        });
-        mobileTitle.forEach((title) => {
-          title.addEventListener("click", function () {
-            EnumerationData(title);
-
-            //Определение активной подтемы
-            try_free_theme.forEach((elem) => {
-              if (elem.trial_theme === document.querySelector(".trial_nested-list_mobile p.active").textContent) {
-                mobileTitle.forEach((arr) => {
-                  if (arr !== elem) arr.classList.remove("active");
-                });
-                title.classList.add("active");
-                SeparationTxt(elem.text).forEach((e) => {
-                  let create_txt = document.createElement('p');
-                  create_txt.innerHTML = e;
-                  text.append(create_txt);
-                  try_free_theme = []
-                })
-                //text.querySelector("p").textContent = SeparationTxt(elem.text);
-              }
-            });
-          });
-        });
-      } //desktop
-      else if ($window.width() >= 960) {
-        const list = document.querySelector(".trial_list");
-        const elem = document.querySelectorAll(".trial_nested-list");
-        if (elem) {
-          elem.forEach((item) => {
-            if (!item.classList.contains("active")) {
-              item.style.display = "none";
-            }
-
-            item.addEventListener("click", function (evt) {
-              item.children.forEach((el) => el.classList.remove("show-active"));
-              evt.target.parentNode.classList.add("show-active");
-
-              if (evt.target instanceof HTMLParagraphElement) {
-                document.querySelector(".trial_description-title").textContent =
-                  evt.target.textContent;
-              }
-            });
-          });
-        }
-
-        if (list) {
-          list.addEventListener("click", (evt) => {
-            if (evt.target.classList.contains("trial_title")) {
-              evt.target.classList.toggle("active");
-
-              evt.target.parentNode
-                .querySelector(".trial_nested-list")
-                .classList.toggle("active");
-              evt.target.parentNode.querySelector(
-                ".trial_nested-list"
-              ).style.display = "";
-              document.querySelector(".trial_description-title").textContent =
-                evt.target.parentNode.querySelector(".show-active").textContent;
-
-              //изменение контента в зависимости от id
-              EnumerationData(evt.target);
-
-              try_free_theme.forEach((elem) => {
-                if (
-                  elem.trial_theme ===
-                  evt.target
-                    .closest(".trial_item")
-                    .querySelector(".show-active").innerText
-                ) {
-                  SeparationTxt(elem.text).forEach((e) => {
-                    let create_txt = document.createElement('p');
-                    create_txt.innerHTML = e;
-                    text.append(create_txt);
-                    try_free_theme = []
-                  })
-                  //text.querySelector("p").textContent = SeparationTxt(elem.text);
-                }
-              });
-
-              [...document.querySelectorAll(".trial_title")].map((el) => {
-                if (el !== evt.target) {
-                  el.classList.remove("active");
-                  el.parentNode
-                    .querySelector(".trial_nested-list")
-                    .classList.remove("active");
-                  el.parentNode.querySelector(
-                    ".trial_nested-list"
-                  ).style.display = "none";
+      mobileTitle.forEach((title) => {
+        title.addEventListener('click', function() {
+          //выделение активного заголовка 
+          activeElement(mobileTitle, title, 'active');
+          mobileSubtitle.forEach((subtitle) => {
+            //смена контента в зависимости от активного подзаголовка
+            if (subtitle.querySelector('p').classList.contains('active')) {
+              dataBase[title.getAttribute('data-key')].forEach((elem) => {
+                if (elem.theme === subtitle.querySelector('p').textContent) {
+                  changeContent(elem);
                 }
               });
             }
-          });
-        }
-      };
-    });
-  });
+          }) 
+        })
+      });
 
-  //начальная отрисовка тем
-  Object.keys(arrayThemes).forEach((key) => {
-    if (list) {
-      list.innerHTML += `
-              <li class="trial_item" id=${key}>
-                              <p id=${key} class="trial_title ${key == 1 ? "active" : ""
-        }">Тема ${key}</p>
-                              <ul class="trial_nested-list ${key == 1 ? "active" : ""
-        }">
-                                  <li class="show-active">
-                                      <p>Общее</p>
-                                  </li>
-                                  <li>
-                                      <p class="trial_list-reference">Рекомендуемая система</p>
-                                  </li>
-                                  <li>
-                                      <p class="trial_list-diary">Дневник питания</p>
-                                  </li>
-                              </ul>
-                          </li>
-              `;
+      mobileSubtitle.forEach((sub) => {
+        sub.addEventListener('click', function() {
+          // выделение активного подзаголовка 
+          mobileSubtitle.forEach((item) => {
+            if (item !== sub && item.querySelector('p').classList.contains('active')) {
+              item.querySelector('p').classList.remove('active');
+            }
+          })
+          sub.querySelector('p').classList.add('active');
+          
+          // смена контента в зависимости от активного заголовка 
+          mobileTitle.forEach((title) => {
+            if (title.classList.contains('active')) {
+              dataBase[title.getAttribute('data-key')].forEach((elem) => {
+                if (elem.theme === sub.querySelector('p').textContent) {
+                  changeContent(elem);
+                }
+              });
+            }
+          })
+        });
+      });
     }
   });
 })();
