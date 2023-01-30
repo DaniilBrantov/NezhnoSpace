179df9866710d00e0f1cac7ce562c6b49e10a442
svetlanael12 <svetlana_el12@mail.ru>
2023-01-30 19:35:39 +0500

    fixed: account show-success

--- js/personal_area.js
+++ js/personal_area.js
@@ -199,7 +199,7 @@ $("#upload_btn").click(function (e) {
       cache: false,
       data: formData,
       success: function (data) {
-        if (data === 'true') {
+        if (typeof data === "boolean") {
           uploadInfoShow(1, 'green', 'Данные успешно сохранены!');
           sessionStorage.clear();
         } else {
