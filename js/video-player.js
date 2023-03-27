e373a39025060097bf17b2c07d84cd15cb18e707
svetlanael12 <svetlana_el12@mail.ru>
2023-03-27 23:30:10 +0500

    refactor: video-player

--- js/video-player.js
+++ js/video-player.js
@@ -1,150 +1,114 @@
 (() => {
-  const videoPlayer = document.querySelector("#video-player");
-  const playVideoBtn = document.querySelector(".video_play_btn");
-  const fullscreenBtn = document.querySelector(".fullscreen-box_video");
-  const volumeVideoIcon = document.querySelectorAll(".volume-video-icon");
-  const moreBtn = document.querySelector(".more-box_video");
-  const speedBtn = document.querySelector(".more-alert_speed");
+  class VideoPlayer {
+    constructor(video) {
+      this.video = video;
+      this.playVideoBtn = document.querySelector(".video_play_btn");
+      this.fullscreenBtn = document.querySelector(".fullscreen-box_video");
+      this.volumeVideoIcon = document.querySelectorAll(".volume-video-icon");
+      this.moreBtn = document.querySelector(".more-box_video");
+      this.speedBtn = document.querySelector(".more-alert_speed");
+      this.progressBarVideo = document.querySelector("#progress-video");
+      this.timerControlsContainer = null;
+      this.myTimeout = null;
+      this.speedTimer = null;
+      this.speedVideoIndexValue = 1;
+    }
+    videoPlay() {
+      this.playVideoBtn.classList.replace("play", "pause");
+      this.video.play();
+    }
+    videoPause() {
+      this.playVideoBtn.classList.replace("pause", "play");
+      this.video.pause();
+    }
+    videoControl() {
+      if (this.timerControlsContainer) {
+        clearTimeout(this.timerControlsContainer);
+      }
+      this.video.paused ? this.videoPlay() : this.videoPause();
+      document.querySelector(".video-controls").style.display = "block";
+      this.timerControlsContainer = setTimeout(() => (
+        document.querySelector(".video-controls").style.display = "none"
+      ), 3000);
+    }
+    setSliderMax() {
+      this.progressBarVideo.max = Math.floor(this.video.duration);
+    }
+    showRangeProgress(rangeInput) {
+      this.videoPause();
+      this.video.style.setProperty("--seek-before-width", (rangeInput.value / rangeInput.max) * 100 + "%");
+      this.setSliderMax();
+    }
+    progress() {
+      this.progressBarVideo.addEventListener("input", (e) => {
+        e.preventDefault();
+        this.showRangeProgress(e.target);
+      });
 
-  const progressBarVideo = document.querySelector("#progress-video");
-
-  if (videoPlayer) {
-    // progress video
-    const setSliderMax = () => {
-      progressBarVideo.max = Math.floor(videoPlayer.duration);
-    };
-
-    const showRangeProgress = (rangeInput) => {
-      videoPause();
-      videoPlayer.style.setProperty(
-        "--seek-before-width",
-        (rangeInput.value / rangeInput.max) * 100 + "%"
-      );
-      setSliderMax();
-    };
-
-    progressBarVideo.addEventListener("input", (e) => {
-      e.preventDefault();
-      showRangeProgress(e.target);
-    });
-
-    progressBarVideo.addEventListener("change", (e) => {
-      e.preventDefault();
-      videoPlayer.currentTime = e.target.value;
-      videoPlay();
-    });
-
-    //duration video
-
-    videoPlayer.addEventListener(
-      "loadeddata",
-      () => {
-        let duration = getTimeCodeFromNum(videoPlayer.duration);
+      this.progressBarVideo.addEventListener("change", (e) => {
+        e.preventDefault();
+        this.video.currentTime = e.target.value;
+        this.videoPlay();
+      });
+    }
+    duration() {
+      this.video.addEventListener("loadeddata", () => {
+        let duration = getTimeCodeFromNum(this.video.duration);
         let i = 0;
 
         while (isNaN(duration)) {
-          duration = videoPlayer.duration;
+          duration = this.video.duration;
           if (i === 10) {
-            document.querySelector(
-              ".duration-video .duration-time-video"
-            ).textContent = "--";
+            document.querySelector(".duration-video .duration-time-video").textContent = "--";
             break;
           } else {
-            document.querySelector(
-              ".duration-video .duration-time-video"
-            ).textContent = getTimeCodeFromNum(videoPlayer.duration);
-            videoPlayer.volume = 1;
+            document.querySelector(".duration-video .duration-time-video").textContent = getTimeCodeFromNum(this.video.duration);
+            this.video.volume = 1;
           }
           i++;
         }
-      },
-      false
-    );
+      }, false);
 
-    setInterval(() => {
-      setSliderMax();
-      videoPlayer.style.setProperty(
-        "--seek-before-width",
-        (progressBarVideo.value / progressBarVideo.max) * 100 + "%"
-      );
-      document.querySelector(
-        ".duration-video .current-time-video"
-      ).textContent = getTimeCodeFromNum(videoPlayer.currentTime);
-    }, 2);
+      setInterval(() => {
+        this.setSliderMax();
+        this.video.style.setProperty("--seek-before-width", (this.progressBarVideo.value / this.progressBarVideo.max) * 100 + "%");
+        document.querySelector(".duration-video .current-time-video").textContent = getTimeCodeFromNum(this.video.currentTime);
+      }, 2);
 
-    videoPlayer.addEventListener("timeupdate", () => {
-      progressBarVideo.value = Math.floor(videoPlayer.currentTime);
-    });
-
-    //plays video
-    function videoPlay() {
-      playVideoBtn.classList.replace("play", "pause");
-      videoPlayer.play();
+      this.video.addEventListener("timeupdate", () => {
+        this.progressBarVideo.value = Math.floor(this.video.currentTime);
+      });
     }
-
-    function videoPause() {
-      playVideoBtn.classList.replace("pause", "play");
-      videoPlayer.pause();
+    click() {
+      this.video.addEventListener("click", () => this.videoControl(), false);
+      this.playVideoBtn.addEventListener("click", () => this.videoControl(), false);
     }
-
-    let timerControlsContainer;
-
-    function videoControl() {
-      clearTimeout(timerControlsContainer);
-      if (videoPlayer.paused) {
-        videoPlay();
-      } else {
-        videoPause();
-      }
-
-      document.querySelector(".video-controls").style.display = "block";
-      timerControlsContainer = setTimeout(
-        () =>
-          (document.querySelector(".video-controls").style.display = "none"),
-        3000
-      );
-    }
-
-    videoPlayer.addEventListener("click", videoControl, false);
-    playVideoBtn.addEventListener("click", videoControl, false);
-
-    //open fullscreen
-    fullscreenBtn.addEventListener("click", () => openFullscreen(), false);
-
-    function openFullscreen() {
-      videoPlayer.controls = "true";
-      videoPlayer.removeEventListener("click", videoControl, false);
-      playVideoBtn.removeEventListener("click", videoControl, false);
-      if (videoPlayer.requestFullscreen) {
-        videoPlayer.requestFullscreen();
+    openFullscreen() {
+      this.video.controls = "true";
+      if (this.video.requestFullscreen) {
+        this.video.requestFullscreen();
       } else if (fullscreenBtnwebkitRequestFullscreen) {
         /* Safari */
-        videoPlayer.webkitRequestFullscreen();
-      } else if (videoPlayer.msRequestFullscreen) {
+        this.video.webkitRequestFullscreen();
+      } else if (this.video.msRequestFullscreen) {
         /* IE11 */
-        videoPlayer.msRequestFullscreen();
+        this.video.msRequestFullscreen();
       }
     }
+    fullscreen() {
+      this.fullscreenBtn.addEventListener("click", () => this.openFullscreen(), false);
 
-    document.addEventListener(
-      "fullscreenchange",
-      () => {
+      document.addEventListener("fullscreenchange", () => {
         if (!document.fullscreenElement) {
-          videoPlayer.removeAttribute("controls");
-          videoPlayer.addEventListener("click", videoControl, false);
-          playVideoBtn.addEventListener("click", videoControl, false);
+          this.video.removeAttribute("controls");
         }
-      },
-      false
-    );
-
-    //volume change
-    let myTimeout;
-
-    function changeVolume(amount) {
-      clearTimeout(myTimeout);
-      videoPlayer.volume = amount;
-
+      }, false);
+    }
+    changeVolume(amount) {
+      if (this.myTimeout) {
+        clearTimeout(this.myTimeout);
+      }
+      this.video.volume = amount;
       if (Number(amount) === 0) {
         document.querySelector(".volume-video").classList.remove("active");
         document.querySelector(".volume-video-none").classList.add("active");
@@ -153,49 +117,30 @@
         document.querySelector(".volume-video-none").classList.remove("active");
       }
 
-      myTimeout = setTimeout(
-        () =>
-          (document.querySelector(".progress_volume-video").style.display =
-            "none"),
-        3000
-      );
+      this.myTimeout = setTimeout(() => (
+        document.querySelector(".progress_volume-video").style.display = "none"
+      ), 3000);
     }
+    volume() {
+      document.querySelector("#volume-video_range").addEventListener("change", (e) => this.changeVolume(e.target.value));
 
-    document
-      .querySelector("#volume-video_range")
-      .addEventListener("change", function () {
-        changeVolume(this.value);
-      });
+      this.volumeVideoIcon.forEach((icon) => {
+        icon.addEventListener("click", function () {
+          if (window.getComputedStyle(document.querySelector(".progress_volume-video")).display === "block") {
+            document.querySelector(".progress_volume-video").style.display = "none";
+          } else if (window.getComputedStyle(document.querySelector(".progress_volume-video")).display === "none") {
+            document.querySelector(".progress_volume-video").style.display = "block";
+            this.myTimeout = setTimeout(() => ( 
+              document.querySelector(".progress_volume-video").style.display = "none"
+            ), 3000);
+          }
+        });
+      });   
 
-    volumeVideoIcon.forEach((icon) => {
-      icon.addEventListener("click", function () {
-        if (
-          window.getComputedStyle(
-            document.querySelector(".progress_volume-video")
-          ).display === "block"
-        ) {
-          document.querySelector(".progress_volume-video").style.display =
-            "none";
-        } else if (
-          window.getComputedStyle(
-            document.querySelector(".progress_volume-video")
-          ).display === "none"
-        ) {
-          document.querySelector(".progress_volume-video").style.display =
-            "block";
-          myTimeout = setTimeout(
-            () =>
-              (document.querySelector(".progress_volume-video").style.display =
-                "none"),
-            3000
-          );
-        }
-      });
-    });
-
-    document.addEventListener(
-      "mousemove",
-      function (e) {
+      document.querySelector("#volume-video_range").addEventListener("change", (e) => this.changeVolume(e.target.value));   
+    }
+    mousemove() {
+      document.addEventListener("mousemove", function (e) {
         if (e.target.nodeName !== "#document") {
           let container = e.target.closest(".video-container");
           if (container) {
@@ -204,48 +149,53 @@
             document.querySelector(".video-controls").style.display = "none";
           }
         }
-      },
-      false
-    );
-
-    //btn more
-    moreBtn.addEventListener("click", function () {
-      if (
-        window.getComputedStyle(document.querySelector(".more-alert_video"))
-          .display === "none"
-      ) {
-        document.querySelector(".more-alert_video").style.display = "block";
-      }
-    });
-
-    //speed video
-    let speedTimer;
-    let speedVideoIndexValue = 1;
-    speedBtn.addEventListener("click", function () {
-      clearTimeout(speedTimer);
+      }, false);
+    }
+    btnMore() {
+      this.moreBtn.addEventListener("click", function () {
+        if (window.getComputedStyle(document.querySelector(".more-alert_video")).display === "none") {
+          document.querySelector(".more-alert_video").style.display = "block";
+        }
+      });
+    }
+    speed() {
       const speedVideoList = [0.75, 1, 1.25];
-      speedVideoIndexValue++;
-      if (speedVideoIndexValue >= speedVideoList.length) {
-        speedVideoIndexValue = 0;
-      }
-      speedBtn.innerText =
-        "Скорость видео: " + speedVideoList[speedVideoIndexValue] + "x";
-      videoPlayer.playbackRate = speedVideoList[speedVideoIndexValue];
-      speedTimer = setTimeout(
-        () =>
-          (document.querySelector(".more-alert_video").style.display = "none"),
-        3000
-      );
-    });
+      this.speedBtn.addEventListener("click", () => {
+        if (this.speedTimer) {
+          clearTimeout(this.speedTimer);
+        }
+        this.speedVideoIndexValue++;
+        if (this.speedVideoIndexValue >= speedVideoList.length) {
+          this.speedVideoIndexValue = 0;
+        }
+        this.speedBtn.innerText = "Скорость видео: " + speedVideoList[this.speedVideoIndexValue] + "x";
+        this.video.playbackRate = speedVideoList[this.speedVideoIndexValue];
+        this.speedTimer = setTimeout(() => (
+          document.querySelector(".more-alert_video").style.display = "none"
+        ), 3000);
+      });
+    }
+    ended() {
+      this.video.addEventListener("ended", () => {
+        this.video.currentTime = 0;
+        this.video.play();
+      }, false);
+    }
+    init() {
+      this.progress();
+      this.duration();
+      this.click();
+      this.fullscreen();
+      this.volume();
+      this.mousemove();
+      this.btnMore();
+      this.speed();
+      this.ended();
+    }
+  }
 
-    //ended video
-    videoPlayer.addEventListener(
-      "ended",
-      function () {
-        videoPlayer.currentTime = 0;
-        videoPlayer.play();
-      },
-      false
-    );
+  if (document.querySelector("#video-player")) {
+    let videoPlayer = new VideoPlayer(document.querySelector("#video-player"));
+    videoPlayer.init();
   }
 })();
