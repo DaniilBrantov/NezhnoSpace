df910c1f639c54b969c09fad53a1734610e27b4c
svetlanael12 <svetlana_el12@mail.ru>
2023-04-23 01:11:51 +0500

    update: add_survey

--- admin/addSurvey.php
+++ admin/addSurvey.php
@@ -16,13 +16,13 @@ checkAdmin();
 
 ?>
 
-<form id="survey-form">
+<!-- <form id="survey-form">
     <label for="poll_name">Название опроса:</label>
     <input type="text" id="poll_name" name="poll_name">
 
     <div class="question-group">
-        <label for="question_1">Вопрос 1:</label>
-        <input type="text" id="question_1" name="questions[]">
+        <label for="question_1" class='question_first-label'>Вопрос 1:</label>
+        <input type="text" id="question_1" name="questions[]" class='question_first-input'>
         <div class="answer-group">
 
             <label id="question_type" for="question_type_1">Тип ответа:</label>
@@ -41,6 +41,55 @@ checkAdmin();
             <button type="button" class="delete-question">Удалить вопрос</button>
         </div>
     </div>
+    <button type="button" class="add-question">Добавить вопрос</button>
+    <button type="submit">Создать опрос</button>
+</form> -->
+
+<form id="survey-form">
+    <div class='survey_wrp-title'>
+        <label>
+            <span class='title-span'>Название опроса:</span>
+            <input type="text" id="poll_name" name="poll_name">
+        </label>
+    </div>
+
+    <div class="question-group">
+        <label class='question_name'>
+            <span class='question_name-span'>Вопрос 1:</span>
+            <input type="text">
+        </label>
+        <div class="answer-group">
+            <span class='answer-group_title'>Тип ответа:</span>
+
+            <div class='answer-group_radio-wrp'>
+                <label class='answer-group_radio'>
+                    <input type="radio" value='radio' checked>
+                    <span class=''>Выбор одного ответа</span>
+                </label>
+                <label class='answer-group_checkbox'>
+                    <input type="radio" value='checkbox'>
+                    <span class=''>Выбор нескольких ответов</span>
+                </label>
+                <label class='answer-group_textarea'>
+                    <input type="radio" value='textarea'>
+                    <span class=''>Свободный ввод текста</span>
+                </label>
+            </div>
+
+            <div class='answer-group_input-answer'>
+                <label>
+                    <span>Ответ:</span>
+                    <input type="text">
+                </label>
+                <button type="button" class="delete-answer">Удалить ответ</button>
+            </div>
+        </div>
+        <button type="button" class="add-answer">Добавить ответ</button>
+        <div class="question-actions">
+            <button type="button" class="delete-question">Удалить вопрос</button>
+        </div>
+    </div>
+
     <button type="button" class="add-question">Добавить вопрос</button>
     <button type="submit">Создать опрос</button>
 </form>
