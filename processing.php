57e79a23d0120ea0565e04a7bd2e7a8e16d5f09f
DaniilBrantov <daniil.brantov04@mail.ru>
2023-06-27 07:20:12 +0300

    tg auth 27/06/23

--- processing.php
+++ processing.php
@@ -177,25 +177,29 @@ class TelegramLogin {
             return [
                 'id' => $_GET['id'],
                 'first_name' => $_GET['first_name'],
+                'last_name' => $_GET['last_name'] ?? '',
                 'username' => $_GET['username'],
-                'photo_url' => $_GET['photo_url']
+                'photo_url' => $_GET['photo_url'],
+                'hash' => $_GET['hash']
             ];
         }
         return false;
     }
 
     public function checkTgUser($tg_user) {
-        $result = [];
-
-        if ($tg_user !== false) {
+        if ($tg_user) {
+            $tg_id = $tg_user['id'];
             $first_name = htmlspecialchars($tg_user['first_name']);
             $last_name = htmlspecialchars($tg_user['last_name']);
             $username = isset($tg_user['username']) ? htmlspecialchars($tg_user['username']) : '';
             $photo_url = isset($tg_user['photo_url']) ? htmlspecialchars($tg_user['photo_url']) : '';
 
             $message = [
+                'id' => $tg_id,
                 'first_name' => $first_name,
-                'last_name' => $last_name
+                'last_name' => $last_name,
+                'hash' => $tg_user['hash'],
+                'auth_date' => strtotime($tg_user["auth_date"]),
             ];
 
             if (!empty($username)) {
@@ -206,14 +210,16 @@ class TelegramLogin {
                 $message['photo_url'] = $photo_url;
             }
 
-            $result['success'] = true;
-            $result['message'] = $message;
+            return [
+                'success' => true,
+                'message' => $message
+            ];
         } else {
-            $result['success'] = false;
-            $result['message'] = "Login with Telegram";
+            return [
+                'success' => false,
+                'message' => 'Login with Telegram'
+            ];
         }
-
-        return $result;
     }
 
     public function saveTelegramUserData($data) {
@@ -223,21 +229,22 @@ class TelegramLogin {
         $sign_check = new UserValidationErrors();
 
         $id = $data['id'];
-        if ($sign_check->getCheckId($id)['status']) {
+        $get_check_id = $sign_check->getCheckId($id);
+
+        if ($get_check_id['status']) {
             $first_name = urldecode($data['first_name']);
-            if (!$sign_check->getName($first_name)) {
+            $get_name = $sign_check->getName($first_name);
+
+            if (!$get_name) {
                 $username = $data['username'];
                 $photo_url = urldecode($data['photo_url']);
                 $auth_date = strtotime($data['auth_date']);
+                return [$get_name, $username, $photo_url, $auth_date];
             } else {
-                return $sign_check->getName($first_name);
+                return $get_name;
             }
         } else {
-            return $sign_check->getCheckId($id)['msg'];
-        }
-
-        if($db->query("INSERT INTO users (name, username, photo_url, last_act) VALUES ('$first_name', '$username', '$photo_url', '$auth_date')")){
-            return 'true';
+            return $get_check_id['msg'];
         }
     }
 
@@ -246,11 +253,37 @@ class TelegramLogin {
 
         $tg_user = $this->getTelegramUserData();
         $result = $this->checkTgUser($tg_user);
-        return $result;
-
         $this->saveTelegramUserData($_GET);
+
+        return $result;
+    }
+
+    public function TgLogin() {
+        $tgData = $this->processTelegramLogin()["message"];
+
+        if ($this->processTelegramLogin()["success"]) {
+            $db = new SafeMySQL();
+            $tg_id = $tgData["id"];
+            $name = $tgData["first_name"];
+            $last_name = $tgData["last_name"];
+            $username = $tgData["username"];
+            $hash = $tgData["hash"];
+            $photo_url = $tgData["photo_url"];
+            $last_act = $tgData["auth_date"];
+
+            $existingUser = $db->getRow("SELECT 1 FROM `users` WHERE `tg_id` = ?s", $tg_id);
+
+            $query = $existingUser
+                ? $db->query("UPDATE `users` SET `name`='$name', `surname`='$last_name', `username`='$username', `password`='$hash', `photo_url`='$photo_url', `last_act`='$last_act' WHERE `tg_id`='$tg_id'")
+                : $db->query("INSERT INTO `users`(`tg_id`, `name`, `surname`, `username`, `password`, `photo_url`, `last_act`) VALUES ('$tg_id', '$name', '$last_name', '$username', '$hash', '$photo_url', '$last_act')");
+
+            return $query !== false;
+        }
+
+        return 'Произошла ошибка. Попробуйте снова';
     }
 }
+
 
 
 
