28c95545ad7537ab1aa7fbb502dcdbb967506ad5
DaniilBrantov <daniil.brantov04@mail.ru>
2023-08-14 14:32:26 +0300

    pay_success

--- libs/cloudpayments/src/Client/Response/PayoutClientResponse.ts
+++
@@ -1,9 +0,0 @@
-import {PayoutResponse, PayoutSuccessResponse} from "../../Api";
-import {ClientResponse} from "../ClientResponse";
-
-export class PayoutClientResponse<T extends PayoutResponse> extends ClientResponse<T> {
-    public isPayoutSuccessResponse(): this is ClientResponse<PayoutSuccessResponse> {
-        const {Model} = this.getResponse();
-        return this.isSuccess() && PayoutClientResponse.has(["TransactionId", "AuthCode"], Model);
-    }
-}
--- libs/cloudpayments/test/src/lib/functions.ts
+++
@@ -1,9 +0,0 @@
-import {BaseResponse} from "../../../src";
-
-export function createResponse<T extends BaseResponse>(data: object, success = true): T {
-    return {
-        Model: data,
-        Success: success,
-        Message: success ? null : "Error message",
-    } as any;
-}
